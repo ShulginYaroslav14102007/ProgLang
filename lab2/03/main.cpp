55bf39ab2698ca0f757f8f56752d7bc7643d556c
ShulginYaroslav14102007 <6638850@mail.ru>
2026-09-25 12:12:51 +0800

    Add files via upload

---
+++ lab2/03/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 1, b = 2;
+    int c = a+++b;
+    
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+    
+    return 0;
+}
